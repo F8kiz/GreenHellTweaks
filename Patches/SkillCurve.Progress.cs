397c7055f9b885421f9f68578db4e9cef3ec42f2
Fakiz <marko.j.bachmann@gmail.com>
2023-03-12 21:58:05 +0100

    Added

---
+++ Patches/SkillCurve.Progress.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using HarmonyLib;
+
+namespace GHTweaks.Patches
+{
+    [HarmonyPatch(typeof(SkillCurve), nameof(SkillCurve.Progress))]
+    internal class SkillCurveProgress
+    {
+        static void Postfix(ref float __result)
+        {
+            Mod.Instance.WriteLog($"Multiply {__result}");
+            __result *= 10;
+        }
+    }
+}
