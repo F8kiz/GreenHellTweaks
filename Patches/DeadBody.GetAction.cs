cb4f7d0d6e6ca70ad947c271d25128964900994e
Fakiz <cs.fakiz@googlemail.com>
2023-03-14 23:54:17 +0100

    Added

---
+++ Patches/DeadBody.GetAction.cs
@@ -0,0 +1,32 @@
+﻿using AIs;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GHTweaks.Patches
+{
+    [HarmonyPatch(typeof(DeadBody), nameof(DeadBody.GetActions))]
+    internal class DeadBodyGetAction
+    {
+        static List<TriggerAction.TYPE> lstActions;
+
+        static void Prefix(List<TriggerAction.TYPE> actions) => lstActions = actions;
+        
+
+        static void Postfix(ref List<TriggerAction.TYPE> actions)
+        {
+            if (lstActions.Count == actions.Count && HasPlayerSomeKindOfBlade())
+                actions.Add(TriggerAction.TYPE.HarvestHold);
+        }
+
+        static bool HasPlayerSomeKindOfBlade()
+        {
+            Player p = Player.Get();
+            Item currentItem = p.GetCurrentItem(Enums.Hand.Right);
+            if (currentItem && (currentItem.m_Info.IsMachete() || currentItem.m_Info.IsAxe()))
+                return true;
+
+            return InventoryBackpack.Get().m_Items.FirstOrDefault(x => x.m_Info.IsMachete() || x.m_Info.IsAxe()) != null;
+        }
+    }
+}
