ed644ec0b5aecb1605ef9fec09616e73d8b19f63
Fakiz <cs.fakiz@googlemail.com>
2024-04-12 00:20:30 +0200

    Added functionality to show the player world position.

---
+++ Patches/MenuDebugTeleport.Show.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+
+using UnityEngine;
+
+namespace GHTweaks.Patches
+{
+    [HarmonyPatchCategory(PatchCategory.Cheats)]
+    [HarmonyPatch(typeof(MenuDebugTeleport), "OnShow")]
+    internal class MenuDebugTeleportShow
+    {
+        static void Postfix(MenuDebugTeleport __instance)
+        {
+            Vector3 worldPosition = Player.Get().GetWorldPosition();
+            __instance.m_PosX.text = worldPosition.x.ToString();
+            __instance.m_PosY.text = worldPosition.y.ToString();
+            __instance.m_PosZ.text = worldPosition.z.ToString();
+        }
+    }
+}
